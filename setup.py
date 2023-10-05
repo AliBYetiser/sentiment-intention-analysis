a48d828b4177271507ec551440e5ec0c4739eb2d
Ali Berkay Yetişer <byetiser@hotmail.com>
2023-10-05 15:19:29 +0300

    Minor changes

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="sentiment-intention-analysis",
-    version="0.1.0",
+    version="0.1",
     packages=find_packages(include=["src", "src.*","src.analysis", "src.analysis.*", "src.utils", "src.utils.*"]),
+    license="MIT License",
+    author="AliBYetiser",
+    author_email="abyetiser@gmail.com",
+    description="Toolkit for sentiment analysis and intention identification in text conversations between agents and customers using zero-shot techniques.",
     install_requires=[
         "transformers",
         "pandas",
